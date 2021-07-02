df5d382ed36447ff4cdad4c814f6a2a639e56288
Maplejoe0 <80821269+Maplejoe0@users.noreply.github.com>
2021-07-02 15:01:32 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,12 +1,8 @@
 package com.example;
 
-
 public class Greeter {
-  
   public Greeter() {
-
   }
-
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
