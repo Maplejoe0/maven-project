ec28ff16e5096083763061ca7871b066f64b89e9
Maplejoe0 <80821269+Maplejoe0@users.noreply.github.com>
2021-07-02 14:52:03 +0200

    Update TestGreeter.java

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -8,6 +8,7 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
+/** {@inheritDoc} */
 public class TestGreeter {
 
   private Greeter greeter;
