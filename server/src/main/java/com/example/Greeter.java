c9abb660187314102d2e8238d53fd55383eddcda
Maplejoe0 <80821269+Maplejoe0@users.noreply.github.com>
2021-07-02 15:04:08 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,10 +1,6 @@
 package com.example;
 import org.junit.Before;
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.containsString;
 
 public class Greeter {
   public Greeter() {
